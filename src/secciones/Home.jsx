6c204788468ab8a424c8aeb92c871bdcd185e764
Virggi <98536685+Virggi@users.noreply.github.com>
2023-05-28 19:37:12 -0300

    Estructura (#3)
    * Estructura
    
    * Imagen Home

--- src/secciones/Home.jsx
+++ src/secciones/Home.jsx
@@ -1,5 +1,6 @@
 import Welcome from '../componentes/Welcome';
 import ButtonWelcome from '../componentes/ButtonWelcome';
+import '../Imagenes/imagenHome.gif';
 import '../css/Home.css';
 
 
@@ -9,6 +10,7 @@ export default function Home (){
         <div className="homeContainer">
         <h1 className="tituloHome">Home</h1>
         <Welcome />
+        <img src={"https://i.pinimg.com/originals/05/bd/e4/05bde4c8cf2e0f2debabd56ae1589e4d.gif"} alt="ImagenHome" className='imagenHome' />
         <ButtonWelcome />
         </div>
         </>
